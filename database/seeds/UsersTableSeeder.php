38e25cb9103d371c70e4085a2539e73546faa773
jw-sprout <jermayne21@gmail.com>
2019-03-26 07:14:13 -0600

    created project. made auth, added user seed data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    // $lname = array(
+    //   'Meadows',
+    // 	'Morin',
+    // 	'Gordon',
+    // 	'Hutchinson',
+    // 	'Fulton'
+    // );
+    // $email = array();
+
+    public function run()
+    {
+      $name = array(
+      'Chester Meadows',
+     	'Lane Morin',
+     	'Merrill Gordon',
+     	'Tanya Hutchinson',
+     	'Isabelle Fulton'
+     );
+      // DB::table('users'->insert([
+      //       'name' => Str::random(10,
+      //       'email' => Str::random(10.'@gmail.com',
+      //       'password' => bcrypt('secret',
+      //   ];
+      // shuffle($name);
+      for ($i = 0; $i < sizeOf($name); $i++){
+        App\Users::create([
+          // 'name' => implode($name[$i]),
+          'name' => $name[$i],
+          'email' => str_replace(' ', '.', $name[$i]. '@gmail.com'),
+          'password' => 'welcome',
+        ]);
+      }
+    }
+}
